77dcf922781eb15cd3c611f1f5387477af93c17c
mypc_wsl <1311585620@qq.com>
2022-11-13 10:46:04 +0800

    去除登录和路由控制 只要展示前端模板

--- src/permission.ts
+++ src/permission.ts
@@ -28,51 +28,54 @@ router.beforeEach(
     next: any
   ) => {
     NProgress.start();
-    // determine whether the user has logged in
-    if (store.state.user.token) {
-      // 已经登录过 不进入登录页 直接回到首页
-      if (to.path === "/login") {
-        next({ path: "/" });
-        NProgress.done();
-      } else {
-        // Check whether the user has obtained his permission roles
-        if (store.state.user.roles.length === 0) {
-          try {
-            // await store.dispatch("GetUserInfo");
-            // const roles = store.state.user.roles;
-            // Generate accessible routes map based on role
-            store.dispatch("GenerateRoutes", ["admin"]);
-            // Dynamically add accessible routes
-            // store.state.permission.dynamicRoutes.forEach((route) => {
-            //   router.addRoute(route);
-            // });
-            // Hack: ensure addRoutes is complete
-            // Set the replace: true, so the navigation will not leave a history record
-            // next({ ...to, replace: true });
 
-            // next(...)一定要有判断逻辑 不然会无限触发
-            next();
-          } catch (err) {
-            store.dispatch("ResetToken");
-            ElMessage.error("err" || "hsa error in router beforeEach");
-            next(`/login?redirect=${to.path}`);
-            NProgress.done();
-          }
-        } else {
-          next();
-        }
-      }
-    } else {
-      // has no token
-      if (whiteList.indexOf(to.path) !== -1) {
-        // in the free login whitelist, go directly
-        next();
-      } else {
-        // Other pages that do not have permission to access are redirected to the login page.
-        next(`/login?redirect=${to.path}`);
-        NProgress.done();
-      }
-    }
+    store.dispatch("GenerateRoutes", ["admin"]);
+    next();
+    // determine whether the user has logged in
+    // if (store.state.user.token) {
+    //   // 已经登录过 不进入登录页 直接回到首页
+    //   if (to.path === "/login") {
+    //     next({ path: "/" });
+    //     NProgress.done();
+    //   } else {
+    //     // Check whether the user has obtained his permission roles
+    //     if (store.state.user.roles.length === 0) {
+    //       try {
+    //         // await store.dispatch("GetUserInfo");
+    //         // const roles = store.state.user.roles;
+    //         // Generate accessible routes map based on role
+    //         store.dispatch("GenerateRoutes", ["admin"]);
+    //         // Dynamically add accessible routes
+    //         // store.state.permission.dynamicRoutes.forEach((route) => {
+    //         //   router.addRoute(route);
+    //         // });
+    //         // Hack: ensure addRoutes is complete
+    //         // Set the replace: true, so the navigation will not leave a history record
+    //         // next({ ...to, replace: true });
+
+    //         // next(...)一定要有判断逻辑 不然会无限触发
+    //         next();
+    //       } catch (err) {
+    //         store.dispatch("ResetToken");
+    //         ElMessage.error("err" || "hsa error in router beforeEach");
+    //         next(`/login?redirect=${to.path}`);
+    //         NProgress.done();
+    //       }
+    //     } else {
+    //       next();
+    //     }
+    //   }
+    // } else {
+    //   // has no token
+    //   if (whiteList.indexOf(to.path) !== -1) {
+    //     // in the free login whitelist, go directly
+    //     next();
+    //   } else {
+    //     // Other pages that do not have permission to access are redirected to the login page.
+    //     next(`/login?redirect=${to.path}`);
+    //     NProgress.done();
+    //   }
+    // }
   }
 );
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,7 +8,13 @@
 <template>
   <div class="login-container">
     <div class="main">
-      <el-form ref="formRef" :model="form" :rules="rules" label-width="120px">
+      <el-form
+        ref="formRef"
+        :model="form"
+        :rules="rules"
+        label-width="80px"
+        v-show="isLogin"
+      >
         <el-form-item label="用户名" prop="username">
           <el-input
             v-model="form.username"
@@ -24,6 +30,7 @@
           <el-form-item label="密码" prop="password">
             <el-input
               v-model="form.password"
+              type="password"
               placeholder="请输入密码"
               @keyup="checkCapslock"
               @blur="capsTooltip = false"
@@ -40,6 +47,73 @@
             >登录</el-button
           >
         </el-form-item>
+        <el-form-item class="right-form-item">
+          <a class="confirm-btn" @click="onTurnRegister">注册</a>
+        </el-form-item>
+      </el-form>
+
+      <el-form
+        ref="formRegisterRef"
+        :model="registerForm"
+        :rules="registerRules"
+        label-width="80px"
+        v-show="!isLogin"
+      >
+        <el-form-item label="昵称" prop="nickname">
+          <el-input
+            v-model="registerForm.nickname"
+            placeholder="请输入昵称"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="用户名" prop="username">
+          <el-input
+            v-model="registerForm.username"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-tooltip
+          :visible="capsTooltip"
+          content="Caps lock is on"
+          placement="right"
+          manual
+        >
+          <el-form-item label="密码" prop="password">
+            <el-input
+              v-model="registerForm.password"
+              type="password"
+              placeholder="请输入密码"
+              @keyup="checkCapslock"
+              @blur="capsTooltip = false"
+              @keyup.enter="register"
+            ></el-input>
+          </el-form-item>
+        </el-tooltip>
+        <el-form-item label="重复密码" prop="rePassword">
+          <el-input
+            v-model="registerForm.rePassword"
+            type="password"
+            placeholder="请输入密码"
+            @keyup.enter="register"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input
+            v-model="registerForm.email"
+            placeholder="请输入邮箱"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            :loading="loading"
+            class="login-btn"
+            type="primary"
+            @click="register"
+            >注册</el-button
+          >
+        </el-form-item>
+        <el-form-item class="right-form-item">
+          <a class="confirm-btn" @click="onTurnLogin">已有账号，直接登录</a>
+        </el-form-item>
       </el-form>
     </div>
   </div>
@@ -48,7 +122,7 @@
 <script lang="ts" setup>
 import { reactive, ref, watch } from "vue";
 import { ElMessage } from "element-plus";
-import { loginApi } from "@/api/admin/adminUser";
+import { loginApi, registerApi } from "@/api/admin/adminUser";
 import { useStore } from "vuex";
 import {
   useRoute,
@@ -62,9 +136,16 @@ interface Form {
   password: string;
 }
 
+interface RegisterForm extends Form {
+  nickname: string;
+  rePassword: string;
+  email: string;
+}
+
 const store = useStore();
 const route = useRoute();
 const router = useRouter();
+
 const getOtherQuery = (query: LocationQuery) => {
   return Object.keys(query).reduce((acc, cur) => {
     if (cur !== "redirect") {
@@ -73,6 +154,7 @@ const getOtherQuery = (query: LocationQuery) => {
     return acc;
   }, {} as LocationQuery);
 };
+
 watch(route, (newVal: RouteLocationNormalizedLoaded) => {
   const query = newVal.query as LocationQuery;
   if (query) {
@@ -82,14 +164,18 @@ watch(route, (newVal: RouteLocationNormalizedLoaded) => {
 });
 
 const formRef = ref();
+const isLogin = ref(true);
+
 const form = reactive<Form>({
-  username: "" as string,
-  password: "" as string,
+  username: "",
+  password: "",
 });
+
 const rules = reactive({
   username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
   password: [{ required: true, message: "请输入密码", trigger: "blur" }],
 });
+
 const loading = ref(false);
 const redirect = ref("");
 // const otherQuery: Dictionary<string> = {};
@@ -118,10 +204,55 @@ const login = async () => {
 };
 
 let capsTooltip = ref(false);
+
 const checkCapslock = (e: KeyboardEvent) => {
   const { key } = e;
   capsTooltip.value =
     key !== null && key.length === 1 && key >= "A" && key <= "Z";
+};
+
+const onTurnRegister = () => {
+  isLogin.value = false;
+};
+
+const formRegisterRef = ref();
+
+const registerForm = reactive<RegisterForm>({
+  nickname: "",
+  username: "",
+  password: "",
+  rePassword: "",
+  email: "",
+});
+
+const registerRules = reactive({
+  nickname: [{ required: true, message: "请输入昵称", trigger: "blur" }],
+  username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+  password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+  rePassword: [{ required: true, message: "请再次输入密码", trigger: "blur" }],
+  email: [{ required: true, message: "请输入邮箱", trigger: "blur" }],
+});
+
+const register = async () => {
+  await formRegisterRef.value.validate(async (valid: boolean) => {
+    if (valid) {
+      loading.value = true;
+      let registerRes = await registerApi(registerForm);
+      if (registerRes.code === 0) {
+        ElMessage.success("注册成功");
+        await store.dispatch("Login", registerRes);
+        router.push({
+          path: redirect.value || "/",
+          query: otherQuery,
+        });
+      }
+      loading.value = false;
+    }
+  });
+};
+
+const onTurnLogin = () => {
+  isLogin.value = true;
 };
 </script>
 
@@ -136,5 +267,12 @@ const checkCapslock = (e: KeyboardEvent) => {
 }
 :deep(.login-btn) {
   width: 100%;
+}
+.right-form-item {
+  float: right;
+}
+.confirm-btn {
+  cursor: pointer;
+  color: #409eff;
 }
 </style>
